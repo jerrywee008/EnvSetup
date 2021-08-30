a0094bbbd9dc03d67911ba00a912f27c2d0e0463
jerrywee008 <jerry.wei008@gmail.com>
2021-08-30 14:24:44 +0800

    Addition of vim configuration

---
+++ scripts/common/config_vim.sh
@@ -0,0 +1,13 @@
+# install sdk manager
+curl -s "https://get.sdkman.io" | bash
+
+# Install Space Vim
+curl -sLf https://spacevim.org/install.sh | bash
+
+# Enable python ide configuration
+pip3 install --user pylint
+pip3 install --user yapf
+pip3 install --user isort
+
+#Enable java ide configuration
+
---
+++ scripts/common/config_zsh.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+#
+chsh -s /bin/zsh
+
+
