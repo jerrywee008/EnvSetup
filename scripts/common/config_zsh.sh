8c38a71e5f35d13c683bcb896dccfdb2017cab3d
feiwei <fei.wei@weride.ai>
2021-09-02 19:41:10 +0800

    zsh configuration

--- scripts/common/config_zsh.sh
+++ scripts/common/config_zsh.sh
@@ -2,4 +2,12 @@
 #
 chsh -s /bin/zsh
 
+# Install autojump
+apt-get install autojump
+# Install autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+#Install syntax highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+
 
