5d7517db4e713e7e400a5225395c4fbd95d78913
feiwei <fei.wei@weride.ai>
2021-09-03 10:38:25 +0800

    Add vscode settings

--- scripts/common/config_zsh.sh
+++ scripts/common/config_zsh.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 #
+zsh
 chsh -s /bin/zsh
 
 # Install autojump
@@ -10,4 +11,5 @@ git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-m
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
 
+cp conf/.zshrc ~/ && source ~/.zshrc
 
--- scripts/linuxwise/init_linux.sh
+++ scripts/linuxwise/init_linux.sh
@@ -5,3 +5,6 @@ sudo apt-get install zsh
 
 # install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# install mycli
+pip3 install mycli
